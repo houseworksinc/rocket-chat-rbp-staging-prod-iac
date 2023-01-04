fdf5944efbe94e70c150be7bf808fa301ea6a97d
shree256 <shreeshan256@gmail.com>
2023-01-04 20:40:13 +0530

    File changes for prod and staging

--- install_rocketchat_staging.sh
+++ install_rocketchat_staging.sh
@@ -4,9 +4,9 @@
 terraform get -update
 
 # Step 2: Create a targeted infrastructure plan for the rocketchat module
-terraform plan -var-file=terraform.tfvars -out="./plans/main.tfplan" -target=module.rocketchat
+terraform plan -var-file=.envs/staging.tfvars -out="./plans/staging.tfplan" -target=module.rocketchat
 
 # Step 3: Apply the infrastructure plan
 read -e -p "Apply the Terraform plan? [Y/N] " YN
 [[ $YN == "n" || $YN == "N" || $YN == "" ]] && exit 0
-terraform apply "./plans/main.tfplan"
+terraform apply "./plans/staging.tfplan"
